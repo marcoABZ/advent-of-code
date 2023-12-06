9347f09c2dfaf18bc7556b3648c2f6a69d4537ce
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-06 08:17:43 -0300

    Moves part-1 files to right folder. Unfinished Part-2

---
+++ 2023/Day-5/Part-2/solution.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+const filePath = './demo.txt';
+
+const lines = fs.readFileSync(filePath, "utf-8").trim().split("\n\n");
+
+const seeds = lines[0].split(":")[1].trim().split(" ").map(seed => Number(seed))
+const finalMapping = [];
+
+getFinalMapping(lines);
+
+let closestSeed;
+let closestLocation = Number.POSITIVE_INFINITY;
+
+for(let i = 0; i < seeds.length; i+=2) {
+  const start = seeds[i];
+  const span = seeds[i+1];
+
+  for(let j = 0; j < span; j++) {
+    const seedNumber = start+j;
+
+    const location = getLocation(seedNumber)
+    if (location < closestLocation) {
+      closestSeed = seedNumber;
+      closestLocation = location;
+    }
+  }
+}
+
+console.log(closestLocation);
+
+function getFinalMapping(maps) {
+  let currentMapIndex = 1;
+
+  while(currentMapIndex < maps.length) {
+    const map = maps[currentMapIndex]
+    const values = map.split(":")[1].trim().split("\n").map(el => el.split(" ").map(v => Number(v)))
+
+    values.forEach(rangeDetails => {
+      const [destination_range_start, source_range_start, range_length] = rangeDetails;
+      finalMapping.push([source_range_start, source_range_start + range_length - 1, destination_range_start - source_range_start]);
+    })
+
+    currentMapIndex += 1
+  }
+
+  finalMapping.sort((a, b) => {
+    if (a[0] !== b[0]) return a[0] - b[0]
+    return a[1] - b[1] 
+  })
+  console.log(finalMapping)
+}
+
+function mergeIntervals(intervals) {
+  
+}
+
+function getLocation(seed) {
+  let value = 0;
+  return seed + finalMapping[seed];
+}
