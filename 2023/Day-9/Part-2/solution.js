0043450ab66e25146ee99580253052c02f270d1a
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-09 09:44:59 -0300

    Day-9 Part-2 solution

--- 2023/Day-9/Part-2/solution.js
+++ 2023/Day-9/Part-2/solution.js
@@ -1,4 +1,44 @@
 const fs = require('fs');
 
-const filePath = '../demo.txt';
-const lines = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+const filePath = '../input.txt';
+const sequences = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+let nextValues = [];
+
+sequences.forEach(sequence => {
+  nextValues.push(getNextValue(sequence));
+})
+
+console.log(nextValues.reduce((a, b) => a + b, 0))
+
+function getNextValue(sequence) {
+  let sequences = [sequence.split(" ").map(value => Number(value))]
+  let currentSequence = sequence.split(" ").map(value => Number(value))
+  
+  while (!isFinal(currentSequence)) {
+    let nextSequence = getDiffSequence(currentSequence)
+    sequences.push(nextSequence)
+    currentSequence = nextSequence
+  }
+
+  sequences[sequences.length - 1] = [0, ...sequences[sequences.length - 1]]
+
+  for (let i = sequences.length - 2; i >= 0; i--) {
+    sequences[i] = [sequences[i][0] - sequences[i+1][0], ...sequences[i]]
+  }
+
+  return sequences[0][0]
+}
+
+function isFinal(sequence) {
+  return sequence.filter(a => a !== 0).length === 0
+}
+
+function getDiffSequence(sequence) {
+  let diffSequence = []
+
+  for(let i = 0; i < sequence.length - 1; i++) {
+    diffSequence.push(sequence[i+1] - sequence[i])
+  }
+
+  return diffSequence
+}
