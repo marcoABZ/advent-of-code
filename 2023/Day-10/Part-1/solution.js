fac47b250cc59a4324584aa4df075b4030a453d8
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-11 09:39:40 -0300

    Day-10 Part-1 Solution + Day-11 input

--- 2023/Day-10/Part-1/solution.js
+++ 2023/Day-10/Part-1/solution.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const filePath = '../demo.txt';
+const filePath = '../input.txt';
 const grid = fs.readFileSync(filePath, "utf-8").trim().split("\n").map(line => Array.from(line));
 
 const startPosition = findStartingPosition(grid)
@@ -38,14 +38,34 @@ const allowedConnections = {
 
 let currentPiece = 'S'
 let currentPosition = startPosition
-console.log(grid)
+let depthGrid = []
+let maxDepth = 0
+
+for (let i = 0; i < grid.length; i++) {
+  let line = [];
+  for (let j = 0; j < grid[0].length; j++) {
+    line.push(Number.POSITIVE_INFINITY);
+  }
+  depthGrid.push(line);
+}
+depthGrid[currentPosition[0]][currentPosition[1]] = 0
 
 allowedConnections[currentPiece].directions.forEach(direction => {
-  console.log(currentPosition, direction)
   if (canConnect(currentPiece, currentPosition, direction)) {
-    const maxDepth = expandDFS(currentPiece, currentPosition, direction)
+    const newPosition = [currentPosition[0] + direction[0], currentPosition[1] + direction[1]]
+    expandDFS(newPosition, direction, 1)
   }
 });
+
+for (let i = 0; i < grid.length; i++) {
+  for (let j = 0; j < grid[0].length; j++) {
+    if (depthGrid[i][j] != Number.POSITIVE_INFINITY) { 
+      maxDepth = Math.max(maxDepth, depthGrid[i][j])
+    }
+  }
+}
+
+console.log(maxDepth)
 
 function findStartingPosition(grid) {
   for (let i = 0; i < grid.length; i++) {
@@ -67,9 +87,24 @@ function canConnect(piece, position, direction) {
   const directions = allowedConnections[piece].directions
   const index = directions.indexOf(direction)
 
+  if (index === -1) return false
+
   return allowedConnections[piece].connections[index].includes(grid[position[0] + direction[0]][position[1] + direction[1]])
 }
 
-function expandDFS(origin, position, direction) {
-  return 0
+function expandDFS(origin, originDirection, depth) {
+  if (depthGrid[origin[0], origin[1]] <= depth) return
+  
+  depthGrid[origin[0]][origin[1]] = Math.min(depthGrid[origin[0]][origin[1]], depth)
+  const piece = grid[origin[0]][origin[1]]
+
+  const directions = allowedConnections[piece].directions
+  directions.forEach(direction => {
+    if (originDirection[0] === -direction[0] && originDirection[1] === -direction[1]) return
+
+    if (canConnect(piece, origin, direction)) {
+      const newPosition = [origin[0] + direction[0], origin[1] + direction[1]]
+      expandDFS(newPosition, direction, depth+1)
+    }
+  }); 
 }
