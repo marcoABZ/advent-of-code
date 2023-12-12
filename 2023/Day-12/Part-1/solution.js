56d9f73b1d79241fa4e26a6b9e84c8f8d8a755eb
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-12 14:46:36 -0300

    Day-12 Part-1 solution

--- 2023/Day-12/Part-1/solution.js
+++ 2023/Day-12/Part-1/solution.js
@@ -1,4 +1,55 @@
 const fs = require('fs');
 
-const filePath = '../demo.txt';
+const filePath = '../input.txt';
 const lines = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+let possibilitiesCount = 0
+
+lines.forEach((line, idx) => {
+  const [springsStatePartial, damagedGroupsSizesPartial] = line.split(" ")
+  const springsState = Array.from(springsStatePartial)
+  const damagedGroupsSizes = damagedGroupsSizesPartial.split(',').map(size => Number(size))
+  let possibilities = [springsState]
+
+  for (let i = 0; i < springsState.length; i++) {
+    if (springsState[i] === '?') {
+      let newPossibilities = []
+
+      possibilities.forEach(val => {
+        newPossibilities.push([...val.slice(0, i), '.', ...val.slice(i+1)])
+        newPossibilities.push([...val.slice(0, i), '#', ...val.slice(i+1)])
+      })
+
+      possibilities = newPossibilities
+    }
+  }
+
+  possibilities.forEach(possibility => {
+    let damagedGroups = []
+    let inGroup = false
+    let count = 0
+
+    for (let i = 0; i < possibility.length; i++) {
+      if (possibility[i] === '#') {
+        inGroup = true
+        count += 1
+      } else if (inGroup) {
+        damagedGroups.push(count)
+        count = 0
+        inGroup = false
+      }
+    }
+    if (count !== 0) damagedGroups.push(count)
+
+    if (damagedGroups.length !== damagedGroupsSizes.length) return
+    
+    for (let i = 0; i < damagedGroups.length; i++) {
+      if (damagedGroups[i] !== damagedGroupsSizes[i]) {
+        return
+      }
+    }
+    
+    possibilitiesCount += 1
+  })
+})
+
+console.log(possibilitiesCount)
