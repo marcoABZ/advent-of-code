d55531749f77bd3691d8ce63e92b346c7ac34174
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-13 09:53:54 -0300

    Day-12 Part-2 solution NOT WORKING. Day-13 inputs

--- 2023/Day-12/Part-2/solution.js
+++ 2023/Day-12/Part-2/solution.js
@@ -2,3 +2,87 @@ const fs = require('fs');
 
 const filePath = '../demo.txt';
 const lines = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+let possibilitiesCount = 0
+
+lines.forEach((line) => {
+  const [springsStatePartial, damagedGroupsSizesPartial] = line.split(" ")
+  const springsState = [
+    ...Array.from(springsStatePartial), '?', 
+    ...Array.from(springsStatePartial), '?',  
+    ...Array.from(springsStatePartial), '?', 
+    ...Array.from(springsStatePartial), '?', 
+    ...Array.from(springsStatePartial)
+  ]
+  const damagedGroupsSizes = damagedGroupsSizesPartial
+    .concat(',', damagedGroupsSizesPartial)
+    .concat(',', damagedGroupsSizesPartial)
+    .concat(',', damagedGroupsSizesPartial) 
+    .concat(',', damagedGroupsSizesPartial) 
+    .split(',').map(size => Number(size))
+
+  console.log(springsState, damagedGroupsSizes)
+  let possibleConfigurations = generatePossibilities(springsState)
+  let validConfigurations = countValidConfigurations(possibleConfigurations, damagedGroupsSizes)
+  possibilitiesCount += validConfigurations
+})
+
+function generatePossibilities(springsState) {
+  let possibilities = [springsState]
+
+  for (let i = 0; i < springsState.length; i++) {
+    if (springsState[i] === '?') {
+      let newPossibilities = []
+
+      possibilities.forEach(val => {
+        newPossibilities.push([...val.slice(0, i), '.', ...val.slice(i+1)])
+        newPossibilities.push([...val.slice(0, i), '#', ...val.slice(i+1)])
+      })
+
+      possibilities = newPossibilities
+    }
+  }
+
+  return possibilities
+}
+
+function countValidConfigurations(possibilities, expectedConfiguration) {
+  function getDamagedGroupSized(possibility) {
+    let damagedGroups = []
+    let inGroup = false
+    let count = 0
+
+    for (let i = 0; i < possibility.length; i++) {
+      if (possibility[i] === '#') {
+        inGroup = true
+        count += 1
+      } else if (inGroup) {
+        damagedGroups.push(count)
+        count = 0
+        inGroup = false
+      }
+    }
+    if (count !== 0) damagedGroups.push(count)
+
+    return damagedGroups
+  }
+
+  let validCount = 0
+
+  possibilities.forEach(possibility => {
+    const damagedGroups = getDamagedGroupSized(possibility)
+
+    if (damagedGroups.length !== expectedConfiguration.length) return
+    
+    for (let i = 0; i < damagedGroups.length; i++) {
+      if (damagedGroups[i] !== expectedConfiguration[i]) {
+        return
+      }
+    }
+    
+    validCount += 1
+  })
+
+  return validCount
+}
+
+console.log(possibilitiesCount)
