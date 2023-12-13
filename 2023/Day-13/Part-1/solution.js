f5620efb4001314c6eb719a04fc31d92642f6292
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-13 10:39:46 -0300

    Day-13 Part-1 solution

--- 2023/Day-13/Part-1/solution.js
+++ 2023/Day-13/Part-1/solution.js
@@ -1,4 +1,62 @@
 const fs = require('fs');
 
-const filePath = '../demo.txt';
-const lines = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+const filePath = '../input.txt';
+const patterns = fs.readFileSync(filePath, "utf-8").trim().split("\n\n").map(pattern => pattern.split("\n"));
+
+const VERTICAL_MULTIPLIER = 100
+const HORIZONTAL_MULTIPLIER = 1
+let summaryValue = 0
+
+patterns.forEach(pattern => {
+  const verticalSplit = findVerticalSplit(pattern)
+  if (verticalSplit) {
+    summaryValue += VERTICAL_MULTIPLIER * verticalSplit
+    return
+  }
+
+  const horizontalSplit = findHorizontalSlit(pattern)
+  if (horizontalSplit) {
+    summaryValue += HORIZONTAL_MULTIPLIER * horizontalSplit
+  }
+})
+
+console.log(summaryValue)
+
+function findVerticalSplit(pattern) {
+  let mirrored = [pattern[0]]
+  let remainingLines = [...pattern.slice(1)]
+
+  while (remainingLines.length > 0) { 
+    let isMirror = true
+
+    for (let i = 0; i < Math.min(mirrored.length, remainingLines.length); i++) {
+      if (mirrored[i] != remainingLines[i]) {
+        isMirror = false
+        break
+      }
+    }
+
+    if (isMirror) { 
+      return mirrored.length
+    } else {
+      mirrored = [remainingLines[0], ...mirrored]
+      remainingLines = [...remainingLines.slice(1)]
+    }
+  }
+}
+
+function findHorizontalSlit(pattern) {
+  const newPattern = []
+
+  for (let i = 0; i < pattern[0].length; i++) {
+    let newString = '';
+
+    for (let j = 0; j < pattern.length; j++) {
+      newString += pattern[j][i]
+    }
+
+    newPattern.push(newString)
+  }
+
+  return findVerticalSplit(newPattern)
+}
