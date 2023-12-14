5a3db08b98d4dc98d5ccd82cdb3542c983ccd78b
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-14 12:20:39 -0300

    Day-14 Part-1 solution

--- 2023/Day-14/Part-1/solution.js
+++ 2023/Day-14/Part-1/solution.js
@@ -1,4 +1,24 @@
 const fs = require('fs');
 
-const filePath = '../demo.txt';
+const filePath = '../input.txt';
 const lines = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+
+const maxLoad = lines.length
+let loadSum = 0
+
+for (let j = 0; j < lines[0].length; j++) {
+  let lastCubeShapedRockIndex = 0
+  let stackedRoundRocks = 0
+
+  for (let i = 0 ; i < lines.length; i++) {
+    if (lines[i][j] === 'O') {
+      loadSum += maxLoad - lastCubeShapedRockIndex - stackedRoundRocks
+      stackedRoundRocks += 1 
+    } else if (lines[i][j] === '#') {
+      lastCubeShapedRockIndex = i + 1
+      stackedRoundRocks = 0
+    }
+  }
+}
+
+console.log(loadSum)
