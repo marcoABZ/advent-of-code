dddca45d47cd394306ba2eed030e2cbbae8650bb
Marco Zulian <marco.ab.zulian@gmail.com>
2023-12-15 10:02:06 -0300

    Day-15 Part-1 solution

--- 2023/Day-15/Part-1/solution.js
+++ 2023/Day-15/Part-1/solution.js
@@ -1,4 +1,19 @@
 const fs = require('fs');
 
-const filePath = '../demo.txt';
-const lines = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+const filePath = '../input.txt';
+const steps = fs.readFileSync(filePath, "utf-8").trim().split(',');
+
+console.log(steps.reduce((prev, curr) => prev + hash(curr), 0))
+
+function hash(step) {
+  let hash = 0;
+
+  for (let i = 0; i < step.length; i++) {
+    const ascii = step.charCodeAt(i)
+    hash += ascii
+    hash *= 17
+    hash %= 256
+  }
+
+  return hash
+}
